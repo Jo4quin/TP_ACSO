e616a1b1c5c528f12c990fbae6ee59586b99f704
Santiago Tarasido <starasido@MacBook-Pro-de-Santiago.local>
2025-03-26 16:38:54 -0300

    agrego los opcodes hasta ORR y empiezo con funcion decode

--- TP1-ARM/src/sim.c
+++ TP1-ARM/src/sim.c
@@ -6,13 +6,21 @@
 
 typedef struct {
     int opcode;
-    int largo;
+    int lenght;
 } tuple_t;
 
 tuple_t opcode_list[30] = {
         {10101011001, 11}, //ADDS extended
-        {3, 4},
-        {5, 6}
+        {1011000100, 10}, //ADDS inmediate (ultimos 2 valores son el shift. hay que implementarlo)
+        {1101000100, 10}, //SUBS inmediate (ultimos 2 valores son el shift. Hay que implentarlos)
+        {11001011001, 11}, //SUBS extended 
+        {11010100010, 11}, //HLT 
+        {11101011001, 11}, //CMP extended
+        {1111000100, 10}, //CMP inmediate (ultimos 2 valores son el shift. Hay que implementarlos)
+        {11101010000, 11}, //ANDS shifted (NO HAY QUE IMPLEMENTAR EL SHIFT. son los anteultimos 2)
+        {11001010000, 11}, //EOR shifted
+        {11110001100, 11}, //ORR shifted
+        {}
     };
 
 typedef struct {
@@ -29,6 +37,13 @@ typedef struct {
 
 decoded_instruction decode_instruction(u_int32_t instruction){
     decoded_instruction decoded;
+    for (int i = 0; i < 30; i++){
+        if ((instruction >> (32-opcode_list[i].lenght)) == opcode_list[i].opcode){
+            decoded.opcode = opcode_list[i].opcode;
+            decoded.type = i;
+            break;
+        }
+    }
 
     return decoded;
 }
