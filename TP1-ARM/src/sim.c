078b9dfb8e61330d61d5d7dcb97a6e78affa6951
Santiago Tarasido <starasido@MacBook-Pro-de-Santiago.local>
2025-03-27 09:42:34 -0300

    implemento funciones para llamar a las instrucciones

--- TP1-ARM/src/sim.c
+++ TP1-ARM/src/sim.c
@@ -10,6 +10,22 @@ typedef struct {
     int type;  // 0 = register, 1 = immediate, 2 = data transfer, 3 = branch, 4 = conditional branch, 5 = inmediate wide
 } tuple_t;
 
+typedef struct {
+    u_int32_t opcode;
+    u_int32_t rm;
+    u_int32_t shamt;
+    u_int32_t rn;
+    u_int32_t rd;
+    u_int32_t ALU_immediate;
+    u_int32_t DT_address;
+    u_int32_t op;
+    u_int32_t rt;
+    u_int32_t BR_address;
+    u_int32_t cond_branch_address;
+    u_int32_t MOV_inmediate;
+    u_int8_t type; // 0 = register, 1 = immediate, 2 = data transfer, 3 = branch, 4 = conditional branch, 5 = inmediate wide
+} decoded_instruction;
+
 tuple_t opcode_list[26] = {
     {10001011001, 11, 0}, // ADD Extended
     {10101011001, 11, 0}, // ADDS Extended
@@ -20,7 +36,7 @@ tuple_t opcode_list[26] = {
     {11001010000, 11, 0}, // EOR Shifted
     {11110001100, 11, 0}, // ORR Shifted
     {1001000100, 10, 1},  // ADD Immediate
-    {1101001101, 10, 1},  // LSR Immediate
+    {1101001101, 10, 1},  // LSR Immediat                                       e
     {1101001101, 10, 1},  // LSL Immediate
     {1111000010, 10, 2},  // LDUR (Load)
     {1111000100, 10, 1},  // CMP Immediate
@@ -38,23 +54,6 @@ tuple_t opcode_list[26] = {
     {10110100, 8, 4},     // CBZ (Conditional Branch)
     {11010010100, 11, 5}, // MOVZ (Inmediate Wide)
 };
-
-
-typedef struct {
-    u_int32_t opcode;
-    u_int32_t rm;
-    u_int32_t shamt;
-    u_int32_t rn;
-    u_int32_t rd;
-    u_int32_t ALU_immediate;
-    u_int32_t DT_address;
-    u_int32_t op;
-    u_int32_t rt;
-    u_int32_t BR_address;
-    u_int32_t cond_branch_address;
-    u_int32_t MOV_inmediate;
-    u_int8_t type; // 0 = register, 1 = immediate, 2 = data transfer, 3 = branch, 4 = conditional branch, 5 = inmediate wide
-} decoded_instruction;
 
 decoded_instruction decode_instruction(u_int32_t instruction){
 
@@ -136,7 +135,6 @@ void run_instruction(decoded_instruction decoded_opcode){
     }
 }
 
-
 void arithmetic_instruction(decoded_instruction decoded_opcode){
     switch (decoded_opcode.opcode){
         case 10001011001:
@@ -165,6 +163,84 @@ void arithmetic_instruction(decoded_instruction decoded_opcode){
             break;
     }
 }
+
+void arithmetic_immediate_instruction(decoded_instruction decoded_opcode){  //hay que ver el tema LSR y LSL
+    switch (decoded_opcode.opcode){
+        case 1001000100:
+            ADD_Immediate(decoded_opcode);
+            break;
+        case 1101001101:
+            LSR_Immediate(decoded_opcode);
+            break;
+        case 1101001101:
+            LSL_Immediate(decoded_opcode);
+            break;
+        case 1111000100:
+            CMP_Immediate(decoded_opcode);
+            break;
+        case 1011000100:
+            ADDS_Immediate(decoded_opcode);
+            break;
+        case 1101000100:
+            SUBS_Immediate(decoded_opcode);
+            break;
+    }
+}
+
+void data_transfer_instruction(decoded_instruction decoded_opcode){
+    switch (decoded_opcode.opcode){
+        case 11111000000:
+            STUR(decoded_opcode);
+            break;
+        case 00111000000:
+            STURB(decoded_opcode);
+            break;
+        case 01111000000:
+            STURH(decoded_opcode);
+            break;
+        case 01111000010:
+            LDURH(decoded_opcode);
+            break;
+        case 00111000010:
+            LDURB(decoded_opcode);
+            break;
+    }
+}
+
+void branch_instruction(decoded_instruction decoded_opcode){
+    switch (decoded_opcode.opcode){
+        case 11010100010:
+            HLT(decoded_opcode);
+            break;
+        case 1101011:
+            BR(decoded_opcode);
+            break;
+        case 000101:
+            B(decoded_opcode);
+            break;
+    }
+}
+
+void conditional_branch_instruction(decoded_instruction decoded_opcode){
+    switch (decoded_opcode.opcode){
+        case 10111001:
+            CBNZ(decoded_opcode);
+            break;
+        case 10110100:
+            CBZ(decoded_opcode);
+            break;
+    }
+}
+
+void inmediate_wide_instruction(decoded_instruction decoded_opcode){
+    switch (decoded_opcode.opcode){
+        case 11010010100:
+            MOVZ(decoded_opcode);
+            break;
+    }
+}
+
+
 
 void process_instruction()
 {
